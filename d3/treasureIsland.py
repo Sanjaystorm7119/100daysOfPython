aab54d89c93cd6c03ed015acff0fe6fedce2127a
Sanjay b <sanjaysrivastavab@gmail.com>
2025-07-18 14:53:09 -0700

    d3 completed

--- d3/treasureIsland.py
+++ d3/treasureIsland.py
@@ -23,11 +23,11 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 
 print("Welcome to the Treasure Island.")
 print("Your mission is to find the treasure.")
-choice1 = input("You're at a crossroad. Where do you want to go? Type 'left' or 'right': ").lower()
+choice1 = input("You're at a crossroad. Where do you want to go? Type 'left' or 'right': ").strip().lower()
 if choice1 == "left":
-    choice2 = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat or 'swim' to swim across: ").lower()
+    choice2 = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat or 'swim' to swim across: ").strip().lower()
     if choice2 == "wait":
-        choice3 = input("You arrive at the island unharmed. There is a house with three doors. One red, one yellow, and one blue. Which color do you choose? ").lower()
+        choice3 = input("You arrive at the island unharmed. There is a house with three doors. One red, one yellow, and one blue. Which color do you choose? ").strip().lower()
         if choice3 == "red":
             print("It's a room full of fire. Game Over.")
         elif choice3 == "yellow":
@@ -40,4 +40,3 @@ if choice1 == "left":
         print("You got attacked by an angry shark. Game Over.")
 else:
     print("You fell into a hole. Game Over.")
-    
